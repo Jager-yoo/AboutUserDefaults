ef9e032861e8c298bd8b37a3a1be7a54bc75130e
Jaeho Yoo <y73447jh@gmail.com>
2024-03-24 21:09:29 +0900

    State vs AppStorage - 토글을 사용한 비교

--- AboutUserDefaults/ContentView.swift
+++ AboutUserDefaults/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+  @State private var isToggleOnAsState: Bool = false // ContentView 가 다시 init 되면 초기화 됨
+
+  @AppStorage("isToggleOn") private var isToggleOn: Bool = false // 유저디폴트 래퍼
+
+  var body: some View {
+    VStack {
+      GroupBox {
+        Text("@State")
+        Toggle("isToggleOn", isOn: $isToggleOnAsState)
+          .labelsHidden()
+          .tint(.red)
+      }
+
+      GroupBox {
+        Text("@AppStorage")
+        Toggle("isToggleOn", isOn: $isToggleOn)
+          .labelsHidden() // Toggle 의 금쪽이 행동 감추기
+          .tint(.cyan) // tint 통해 토글의 컬러도 변경 가능
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
