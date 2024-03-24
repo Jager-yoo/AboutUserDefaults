26f42e548baf54e7f4be27feae15393acd499157
Jaeho Yoo <y73447jh@gmail.com>
2024-03-25 01:52:05 +0900

    View 에서 직접 사용하는 UserDefaults 불편함

--- AboutUserDefaults/ContentView.swift
+++ AboutUserDefaults/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
 
   @State private var isToggleOnAsState: Bool = false // ContentView 가 다시 init 되면 초기화 됨
 
-  @AppStorage("isToggleOn") private var isToggleOn: Bool = false // 유저디폴트 래퍼
+  @AppStorage("isToggleOn") private var isToggleOn: Bool = false // 유저디폴트 래퍼 - 변화를 계속 감지한다.
+
+  @State private var isToggleOnMirror = UserDefaults.standard.bool(forKey: "isToggleOn") // 변화 감지 못 함
 
   var body: some View {
     VStack {
@@ -27,6 +29,18 @@ struct ContentView: View {
         Toggle("isToggleOn", isOn: $isToggleOn)
           .labelsHidden() // Toggle 의 금쪽이 행동 감추기
           .tint(.cyan) // tint 통해 토글의 컬러도 변경 가능
+      }
+
+      GroupBox {
+        Text("UserDefaults")
+        Toggle("isToggleOn", isOn: $isToggleOnMirror)
+          .labelsHidden() // Toggle 의 금쪽이 행동 감추기
+          .tint(.mint) // tint 통해 토글의 컬러도 변경 가능
+          .onChange(of: isToggleOnMirror) { oldValue, newValue in
+            if oldValue != newValue {
+              UserDefaults.standard.set(newValue, forKey: "isToggleOn")
+            }
+          }
       }
     }
   }
