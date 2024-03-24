63820c9b82dcd3bc250783600e88a4a3a5ac0f84
Jaeho Yoo <y73447jh@gmail.com>
2024-03-24 20:35:56 +0900

    Initial Commit

---
+++ AboutUserDefaults/AboutUserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutUserDefaultsApp.swift
+//  AboutUserDefaults
+//
+//  Created by 유재호 on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutUserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutUserDefaults/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AboutUserDefaults
+//
+//  Created by 유재호 on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
