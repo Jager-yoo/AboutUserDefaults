ceb0f5267774fd899b0802129c1d09ad6b2fbd41
Jaeho Yoo <y73447jh@gmail.com>
2024-03-25 23:45:56 +0900

    2) SwiftUI 와 UserDefaults 의 불편한 어울림

--- AboutUserDefaults/ContentView.swift
+++ AboutUserDefaults/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
 
-  @State private var num: Int = .zero
+  @State private var num: Int = UserDefaults.standard.integer(forKey: "nothing") // 크래시가 날까?
 
   var body: some View {
     VStack {
@@ -19,7 +19,12 @@ struct ContentView: View {
       }
 
       Button("Up!") {
-        num += 1
+        num += 1 // 저장이 될까? -> 안 됨. @State 값을 바꾼거지 유저디폴트를 건드린 게 아님.
+        UserDefaults.standard.set(num, forKey: "nothing") // 이러면 됨
+        // 무슨 리스크가 있을까?
+        // 1. 순서가 바뀌면 크리티컬 하다.
+        // 2. 매번 이렇게 수동으로 set 을 해줘야 한다.
+        // 3. key 값을 하드코딩하기 때문에 typo 리스크가 있다.
       }
       .buttonStyle(.bordered)
     }
