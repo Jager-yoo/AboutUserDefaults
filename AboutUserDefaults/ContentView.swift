6c6f86c5f4e0e72ba9cbe176a6b7bc25b0ed82ce
Jaeho Yoo <y73447jh@gmail.com>
2024-03-26 00:18:08 +0900

    3) Toggle 하고 UserDefaults 사용하기

--- AboutUserDefaults/ContentView.swift
+++ AboutUserDefaults/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
 
-  @State private var num: Int = UserDefaults.standard.integer(forKey: "nothing") // 크래시가 날까?
+  @State private var num: Int = UserDefaults.standard.integer(forKey: "nothing")
+
+  @State private var isToggleOn: Bool = UserDefaults.standard.bool(forKey: "abc")
 
   var body: some View {
     VStack {
@@ -19,14 +21,20 @@ struct ContentView: View {
       }
 
       Button("Up!") {
-        num += 1 // 저장이 될까? -> 안 됨. @State 값을 바꾼거지 유저디폴트를 건드린 게 아님.
-        UserDefaults.standard.set(num, forKey: "nothing") // 이러면 됨
-        // 무슨 리스크가 있을까?
-        // 1. 순서가 바뀌면 크리티컬 하다.
-        // 2. 매번 이렇게 수동으로 set 을 해줘야 한다.
-        // 3. key 값을 하드코딩하기 때문에 typo 리스크가 있다.
+        num += 1
+        UserDefaults.standard.set(num, forKey: "nothing")
       }
       .buttonStyle(.bordered)
+
+      GroupBox {
+        Toggle("abc", isOn: $isToggleOn) // 저장이 될까? -> 안 됨! 바인딩이라 메서드도 못 씀. 이럴 땐 어떻게 해? -> onChange 사용
+          .labelsHidden()
+          .onChange(of: isToggleOn) { oldValue, newValue in
+            if oldValue != newValue {
+              UserDefaults.standard.set(newValue, forKey: "abc")
+            }
+          }
+      }
     }
   }
 }
